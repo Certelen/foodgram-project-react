ade9fd9e2942678a3602063cb4eb28d8f15dec5e
Дмитрий Коломейцев <dst98@yandex.ru>
2023-06-09 16:27:36 +0300

    Upd #3

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -42,7 +42,7 @@ class RecipesViewSet(ModelViewSetWithOutPut):
     def action_post_delete(self, model, pk):
         recipe = self.get_object()
         user = self.request.user
-        obj = model.objects.filter(recipe=recipe, user=user)
+        obj = model.objects.filter(recipe_id=recipe.id, user=user)
         if self.request.method == 'DELETE':
             if obj.exists():
                 obj.delete()
